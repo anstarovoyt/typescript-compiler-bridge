b669f31504003aa6a46326ee43cf30588c0dae00
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-12-11 19:56:29 +0300

    files for test perfomance

---
+++ java-typescript-compiler-bridge/src/main/java/an/samples/bridge/TypeScriptCompiler.java
@@ -0,0 +1,8 @@
+package an.samples.bridge;
+
+
+import java.util.List;
+
+public interface TypeScriptCompiler {
+    List<String> compile(String filePath);
+}
--- java-typescript-compiler-bridge/src/test/java/an/samples/MainTest.java
+++ java-typescript-compiler-bridge/src/test/java/an/samples/MainTest.java
@@ -1,13 +1,32 @@
 package an.samples;
 
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainTest {
 
     @Test
-    public void test() {
+    public void testTryGetFile() throws URISyntaxException {
 
-        assert true;
+        Assert.assertTrue(Files.exists(Paths.get(getFileUri("ts-files/tsc.ts"))));
+    }
+
+
+    private URI getFileUri(String path) {
+        URL url = Main.class.getClassLoader().getResource(path);
+        Assert.assertNotNull(url);
+
+        try {
+            return url.toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 }
