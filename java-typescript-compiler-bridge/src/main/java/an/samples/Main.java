e211037369abcec6b349da845aa8bc9d6211a8f8
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-12-11 19:43:43 +0300

    add simple files

--- java-typescript-compiler-bridge/src/main/java/an/samples/Main.java
+++ java-typescript-compiler-bridge/src/main/java/an/samples/Main.java
@@ -1,8 +1,6 @@
 package an.samples;
 
-/**
- * Created by anstarovoyt on 11/12/14.
- */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
---
+++ java-typescript-compiler-bridge/src/test/java/an/samples/MainTest.java
@@ -0,0 +1,13 @@
+package an.samples;
+
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+
+        assert true;
+    }
+}
