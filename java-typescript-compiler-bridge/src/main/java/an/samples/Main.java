d9d7b93d55d3eac5216a0f9d2148060f38c80a58
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-12-11 19:39:59 +0300

    add files

---
+++ java-typescript-compiler-bridge/src/main/java/an/samples/Main.java
@@ -0,0 +1,11 @@
+package an.samples;
+
+/**
+ * Created by anstarovoyt on 11/12/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
